cce4c9cfdd06d401a89adef6fb2d99d6f14031c6
Abhinav Kumar Sinha <abhiks3112@gmail.com>
2019-08-26 01:36:22 +0530

    Added platform check and respective code

--- components/MainButton.js
+++ components/MainButton.js
@@ -3,24 +3,41 @@ import {
  View,
  Text,
  StyleSheet,
- TouchableOpacity
+ TouchableOpacity,
+ TouchableNativeFeedback,
+ Platform
 } from 'react-native';
 import Color from '../constants/colors';
 
 const MainButton = (props) => {
     const { style, children, onPress } = props;
+    // Captial letter variable can hold components and be used in
+    // JSX as tag and not small letter beginning variable
+    let ButtonComponent = TouchableOpacity;
+    // TouchableNativeFeedback is available from android OS version
+    // 21 and onwards
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        ButtonComponent = TouchableNativeFeedback;
+    }
     return (
-        <TouchableOpacity onPress={onPress} activeOpacity={0.6}>
-            <View style={{ ...styles.buttonContainerStyle, ...style }}>
-                <Text style={styles.buttonTextStyle}>
-                    {children}
-                </Text>
-            </View>
-        </TouchableOpacity>
+        <View style={styles.buttonRootContainer}>
+            <ButtonComponent onPress={onPress} activeOpacity={0.6}>
+                <View style={{ ...styles.buttonContainerStyle, ...style }}>
+                    <Text style={styles.buttonTextStyle}>
+                        {children}
+                    </Text>
+                </View>
+            </ButtonComponent>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
+    buttonRootContainer: {
+        borderRadius: 25,
+        // Child component part going outside this view will be clipped eg ripple effect shadow
+        overflow: 'hidden'
+    },
     buttonContainerStyle: {
         backgroundColor: Color.primary,
         alignContent: 'center',
